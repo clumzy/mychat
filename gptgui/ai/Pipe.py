0aa3a0445ab54ba96967d70bc2622f664af2be31
clumzy <george.pied@gmail.com>
2023-04-21 23:00:46 +0200

    Update Pipe.py

--- gptgui/ai/Pipe.py
+++ gptgui/ai/Pipe.py
@@ -26,24 +26,10 @@ class WikiPipe():
             +str(round(self._mind_bot.token_use/4097.0, 2)))
     
     def add_user_prompt(self, prompt:str):
-        self._last_user_message = prompt
         self._wiki_bot.add_user_prompt(prompt)
         self._mind_bot.add_user_prompt(prompt)
 
     def return_answer(self):
         wiki_bot_result = loads(self._wiki_bot.return_answer())
         print(wiki_bot_result)
-        mind_package = {"role":"assistant", "content":""}
-        if wiki_bot_result["RECHERCHE"] == "OUI":
-            print(wiki_bot_result["QUERY"])
-            wiki_article_summary = wikipedia.page(wiki_bot_result["QUERY"]).summary
-            mind_package["content"] = (
-                "[CONTENU EN COURS DE RECEPTION]"
-                +"\n[MEMOIRE CHARGEE]"
-                +"\n[AFFICHAGE DES INFORMATIONS EN COURS]"
-                +wiki_article_summary
-                +"\n[REPRISE DU CHAT]")
-            return self._mind_bot.return_answer(
-                self._mind_bot.get_messages().append(mind_package))
-        else:
-            return self._mind_bot.return_answer()
+        return self._mind_bot.return_answer()
