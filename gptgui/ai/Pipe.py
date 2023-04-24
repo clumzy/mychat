5d8c63f60babd4fe3d2f1884a6ad6bc0843a5842
ClumZy <george.pied@gmail.com>
2023-04-24 19:01:08 +0200

    reworked the tool workflow

--- gptgui/ai/Pipe.py
+++ gptgui/ai/Pipe.py
@@ -1,23 +1,14 @@
-from numpy import append
-import wikipedia
-wikipedia.set_lang("fr")
 from wit import Wit
-from json import loads, dumps
-import pyowm
-
-from meteofrance_api import MeteoFranceClient
-
-from datetime import datetime
+from json import dumps
 
 import os
 
-from json import loads
-
 from .Chatbot import Chatbot
+from ..services.WeatherService import WeatherService
 
 class WitPipe():
     def __init__(self,):
-        prompt_path = "D:\\George\\Documents\\Envs\\mychat\\prompting"
+        prompt_path = "/Users/george.pied/Code/clumzy/mychat/prompting/"
         self._mind_bot= Chatbot(
             sys_prompt=os.path.join(prompt_path,"sysprompt.txt"))
         self._wit = Wit("7ZDABHS6FCZTN5LL753LOCGTI7HAOBZW")
@@ -32,33 +23,9 @@ class WitPipe():
             return None
         elif outcome["intents"][0]["name"] == "get_meteo":
             print("meteo")
-            try:
-                date = outcome["entities"]["wit$datetime:datetime"][0]["value"]
-            except KeyError: date = "None"
-            try:
-                location = outcome["entities"]["wit$location:location"][0]["value"]
-            except KeyError: location = "Paris"
-            print(date)
-            client = MeteoFranceClient()
-            list_places = client.search_places(location)
-            my_place = list_places[0]
-            print(my_place)
-            my_place_weather_forecast = client.get_forecast_for_place(my_place)
-            today = datetime.now()
-            if date != "None":
-                date = datetime.strptime(date[:10]+"-23-59", "%Y-%m-%d-%H-%M")
-            else:
-                date = datetime.now()
-            diff = date-today
-            print(diff.days)
-            my_place_daily_forecast = my_place_weather_forecast.daily_forecast
-            if diff.days < 15:
-                package = my_place_daily_forecast[diff.days]
-            else:
-                package = "Impossible de donner une prédiction météo au delà de 15 jours. Veuillez-en informer l'utilisateur."
-            return str(package)
+            meteo = WeatherService(outcome=outcome,)
+            return meteo.get_weather_package()
 
-    
     def _memory_upload(self, memory_package:str):
         upload = (
             "[UPLOAD MEMOIRE EN COURS]\n"
---
+++ gptgui/services/WeatherService.py
@@ -0,0 +1,33 @@
+from meteofrance_api import MeteoFranceClient
+from datetime import datetime
+
+class WeatherService():
+    def __init__(self, outcome:dict, default_city:str = "Paris") -> None:
+        self._today = datetime.now()
+        self._client = MeteoFranceClient()
+        self._outcome = outcome
+        self._default_city = default_city
+        try:
+            wit_date_string = self._outcome["entities"]["wit$datetime:datetime"][0]["value"]
+        except KeyError: wit_date_string = "None"
+        if wit_date_string != "None":
+            self._date = datetime.strptime(wit_date_string[:10]+"-23-59", "%Y-%m-%d-%H-%M")
+        else: self._date = datetime.now()
+        try:
+            self._location = self._outcome["entities"]["wit$location:location"][0]["value"]
+        except KeyError: self._location = "Paris"
+        pass
+
+    def get_weather_package(self,):
+        list_places = self._client.search_places(self._location)
+        my_place = list_places[0]
+        my_place_weather_forecast = self._client.get_forecast_for_place(my_place)
+        diff = self._date-self._today
+        my_place_daily_forecast = my_place_weather_forecast.daily_forecast
+        if diff.days < 15:
+            package = my_place_daily_forecast[diff.days]
+        else:
+            package = "Impossible de donner une prédiction météo au delà de 15 jours. Veuillez-en informer l'utilisateur."
+        return str(package)
+    
+    
